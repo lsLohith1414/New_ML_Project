139351a9b5e489b67f8a3c03e468190063a000c3
lsLohith1414 <lohithls14@gmail.com>
2025-07-05 14:07:56 +0530

    hyper parameter

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ from src.exception import custom_exception
 from src.logger import logging
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 import dill   # to create the Pikcal file
 
@@ -28,23 +29,52 @@ def save_object(file_path,obj):
 
 
 
-def evaluate_model(X_train, y_train, X_test, y_test, models):
-    try:
-        report = {}
+# def evaluate_model(X_train, y_train, X_test, y_test, models):
+#     try:
+#         report = {}
 
-        for model_name, model in models.items():
-            model.fit(X_train, y_train)  # Train model
+#         for model_name, model in models.items():
+#             model.fit(X_train, y_train)  # Train model
 
-            y_train_prediction = model.predict(X_train)
-            y_test_prediction = model.predict(X_test)
+#             y_train_prediction = model.predict(X_train)
+#             y_test_prediction = model.predict(X_test)
 
-            train_model_score = r2_score(y_train, y_train_prediction)
-            test_model_score = r2_score(y_test, y_test_prediction)
+#             train_model_score = r2_score(y_train, y_train_prediction)
+#             test_model_score = r2_score(y_test, y_test_prediction)
 
-            report[model_name] = test_model_score
+#             report[model_name] = test_model_score
 
-        return report   
+#         return report   
 
             
+#     except Exception as e:
+#         raise custom_exception(e,sys)    
+
+
+def evaluate_model(X_train,y_train,X_test,y_test,models,params):
+    try:
+        report={}
+        for i in range(len(models)):
+            
+            model=list(models.values())[i]
+            para=params[list(models.keys())[i]]
+            
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+            
+            
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+            
+            y_train_pred=model.predict(X_train)
+            y_test_pred=model.predict(X_test)
+            
+            train_model_score=r2_score(y_train, y_train_pred)
+            test_model_score=r2_score(y_test, y_test_pred)
+            
+            report[list(models.keys())[i]]=test_model_score
+     
+
+        return report
     except Exception as e:
-        raise custom_exception(e,sys)    
+        raise custom_exception(e,sys)
